6a399f9b1f78fef33ecd06467c711e61dfcf55e2
Markus Flodmark <markus.flodmark@gmail.com>
2018-02-13 11:40:25 +0100

    Win = canvas not clickable

--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,10 @@ $("#refresh").click(function () {
     var canvas = document.getElementById("grid")
     var turn = document.getElementById("turn");
     var winner = document.getElementById("winner");
-    
+
     winner.innerText = "?"
+    winner.style.color = ""
+    winner.style.border = ""
     turn.innerText = 0;
 
     game = new Game();
@@ -22,6 +24,7 @@ window.onload = function () {
 
 function HandleClick(e) {
     var canvas = document.getElementById("grid");
+    var winner = document.getElementById("winner");
     var turn = document.getElementById("turn");
     var c = canvas.getContext("2d");
     var boxSize = canvas.width / 10;
@@ -32,74 +35,77 @@ function HandleClick(e) {
     var cell = document.getElementById("clicked-cell");
     var cellNr = GetCell(x, y, boxSize);
     cell.innerText = cellNr
+    if (winner.innerText == "?") {
+        if (game.grid[cellNr - 1]["used"] == false) {
+            // Update grid used 
+            game.grid[cellNr - 1]["used"] = true;
+            document.addEventListener('click', ToggleClickState, true);
 
-    if (game.grid[cellNr - 1]["used"] == false) {
-        // Update grid used 
-        game.grid[cellNr - 1]["used"] = true;
-        document.addEventListener('click', ToggleClickState, true);
+            if (turn.innerText % 2 == 0) {
+                // Update grid player
+                game.grid[cellNr - 1]["player"] = 1;
 
-        if (turn.innerText % 2 == 0) {
-            // Update grid player
-            game.grid[cellNr - 1]["player"] = 1;
-
-            // Create X        
-            c.beginPath();
-            c.strokeStyle = "blue";
-            var counter = 0;
-            var interval = 10;
-            var padding = 10;
-
-            var intervalId = setInterval(function () {
-                counter += 1;
-                c.moveTo(x + padding, y + padding);
-                c.lineTo(x + counter + padding, y + counter + padding);
-                c.stroke();
-
-                if (counter + padding * 2 == boxSize) {
-                    clearInterval(intervalId)
-                    var counter2 = 0;
-                    var intervalId2 = setInterval(function () {
-                        counter2 += 1;
-                        c.moveTo(x + boxSize - padding, y + padding);
-                        c.lineTo(x + boxSize - counter2 - padding, y + counter2 + padding);
-                        c.stroke();
-
-                        if (counter2 + padding * 2 == boxSize) {
-                            clearInterval(intervalId2);
-                            document.removeEventListener('click', ToggleClickState, true);
-                            CheckWinner(cellNr, 1);
-                        }
-                    }, interval);
-                }
-            }, interval);
-            c.closePath();
-        } else {
-            // Update grid player
-            game.grid[cellNr - 1]["player"] = 2;
-
-            // Create O                
-            var counter3 = 0;
-
-            var intervalId3 = setInterval(function () {
-                counter3 += 0.5;
-                var start = 0;
-
-                c.clearRect(x + 5, y + 5, boxSize - 10, boxSize - 10)
+                // Create X        
                 c.beginPath();
-                c.strokeStyle = "green";
-                c.arc(x + boxSize / 2, y + boxSize / 2, boxSize / 3, start, counter3);
-                c.stroke();
-                c.closePath();
+                c.strokeStyle = "blue";
+                var counter = 0;
+                var interval = 10;
+                var padding = 10;
 
-                if (counter3 > 2 * Math.PI) {
-                    clearInterval(intervalId3);
-                    document.removeEventListener('click', ToggleClickState, true);
-                    CheckWinner(cellNr, 2);
-                }
-            }, 40);
+                var intervalId = setInterval(function () {
+                    counter += 1;
+                    c.moveTo(x + padding, y + padding);
+                    c.lineTo(x + counter + padding, y + counter + padding);
+                    c.stroke();
+
+                    if (counter + padding * 2 == boxSize) {
+                        clearInterval(intervalId)
+                        var counter2 = 0;
+                        var intervalId2 = setInterval(function () {
+                            counter2 += 1;
+                            c.moveTo(x + boxSize - padding, y + padding);
+                            c.lineTo(x + boxSize - counter2 - padding, y + counter2 + padding);
+                            c.stroke();
+
+                            if (counter2 + padding * 2 == boxSize) {
+                                clearInterval(intervalId2);
+                                document.removeEventListener('click', ToggleClickState, true);
+                                CheckWinner(cellNr, 1);
+                            }
+                        }, interval);
+                    }
+                }, interval);
+                c.closePath();
+            } else {
+                // Update grid player
+                game.grid[cellNr - 1]["player"] = 2;
+
+                // Create O                
+                var counter3 = 0;
+
+                var intervalId3 = setInterval(function () {
+                    counter3 += 0.5;
+                    var start = 0;
+
+                    c.clearRect(x + 5, y + 5, boxSize - 10, boxSize - 10)
+                    c.beginPath();
+                    c.strokeStyle = "green";
+                    c.arc(x + boxSize / 2, y + boxSize / 2, boxSize / 3, start, counter3);
+                    c.stroke();
+                    c.closePath();
+
+                    if (counter3 > 2 * Math.PI) {
+                        clearInterval(intervalId3);
+                        document.removeEventListener('click', ToggleClickState, true);
+                        CheckWinner(cellNr, 2);
+                    }
+                }, 40);
+            }
+            turn.innerText = Number(turn.innerText) + 1;
         }
-        turn.innerText = Number(turn.innerText) + 1;
     }
+
+
 }
 
 function GetCell(x, y, boxSize) {
@@ -112,8 +118,8 @@ function GetCell(x, y, boxSize) {
 function CheckWinner(cellNr, player) {
     var row = Math.ceil((cellNr / 10))
     var col = Math.ceil((cellNr % 10))
-    console.log("Row = " + row)
-    console.log("Col = " + col)
+    // console.log("Row = " + row)
+    // console.log("Col = " + col)
 
     // Check winner in row
     var times = 0
@@ -121,7 +127,7 @@ function CheckWinner(cellNr, player) {
         if (Math.ceil(((i + 1) / 10)) == row) {
             if (p["player"] == player) {
                 times += 1
-                if (times == 5) {Winner(player)}
+                if (times == 5) { Winner(player) }
             } else {
                 times = 0
             }
@@ -134,7 +140,7 @@ function CheckWinner(cellNr, player) {
         if (Math.ceil(((i + 1) % 10)) == col) {
             if (p["player"] == player) {
                 times += 1
-                if (times == 5) {Winner(player)}
+                if (times == 5) { Winner(player) }
             } else {
                 times = 0
             }
@@ -181,7 +187,7 @@ function Winner(player) {
     winner.innerText = "Spelare " + player
     winner.style.color = "green"
     winner.style.border = "5px solid red"
-    canvas.addEventListener('click', ToggleClickState, true);
+
 }
 
 function ToggleClickState(e) {
